222df95376bafe2fe0075a33a38fdd03bcaa000b
ferdiannada <ferdian.nadda@gmail.com>
2019-10-01 07:46:55 +0700

    revisi tampilan bagian testimoni pelanggan menjadi foto karyawan, bagian blog menjadi galeri foto

--- application/views/index.php
+++ application/views/index.php
@@ -309,19 +309,118 @@
 </section>
 <!-- #pricing1 end -->
 
-
-<!-- Blog Grid
+<!-- Testimonial #2
 ============================================= -->
-<section id="blog" class="blog blog-grid pb-100 bg-overlay bg-overlay-dark bg-parallax">
+<section id="testimonial2" class="testimonial testimonial-1 bg-overlay bg-overlay-dark bg-parallax text-center">
   <div class="bg-section">
-    <img src="<?= base_url() ?>assets/web_profile/images/background/2.jpg" alt="Background" />
+    <img src="<?= base_url() ?>assets/web_profile/images/testimonial/bg-1.jpg" alt="Background" />
   </div>
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
-        <div class="heading text--center mb-70">
-          <h2 class="heading--title text-white">Galeri</h2>
+        <div class="text--center heading mb-70">
+          <h2 class="heading--title color-white">Karyawan Kami</h2>
+          <p class="heading--desc mb-0 color-gray">Kami memiliki 6 karyawan</p>
+          <div class="divider--line divider--center"></div>
+        </div>
+      </div>
+      <!-- .col-md-8 end -->
+    </div>
+    <!-- .row end -->
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+        <div id="testimonial-carousel" class="carousel carousel-dots carousel-white" data-slide="3" data-slide-rs="1" data-autoplay="false" data-nav="false" data-dots="true" data-space="30" data-loop="true" data-speed="800">
+          <!-- Testimonial #1 -->
+          <div class="testimonial-panel">
+            <div class="testimonial--meta">
+              <div class="testimonial--meta-img">
+                <img src="<?= base_url() ?>assets/web_profile/images/testimonial/1.png" alt="Testimonial Author">
+              </div>
+            </div>
+            <!-- .testimonial-meta end -->
+            <div class="testimonial--body">
+              <p>“Proin gravida nibh vel velit auctor aliquet aenean sollidin, lorem quis bibendum auctor nisi elit.”</p>
+            </div>
+            <!-- .testimonial-body end -->
+            <div class="testimonial--meta-content">
+              <h4>Ryan Printz</h4>
+            </div>
+          </div>
+          <!-- .testimonial-panel end -->
 
+          <!-- Testimonial #2 -->
+          <div class="testimonial-panel">
+            <div class="testimonial--meta">
+              <div class="testimonial--meta-img">
+                <img src="<?= base_url() ?>assets/web_profile/images/testimonial/2.png" alt="Testimonial Author">
+              </div>
+            </div>
+            <!-- .testimonial-meta end -->
+            <div class="testimonial--body">
+              <p>“Proin gravida nibh vel velit auctor aliquet aenean sollidin, lorem quis bibendum auctor nisi elit.”</p>
+            </div>
+            <!-- .testimonial-body end -->
+            <div class="testimonial--meta-content">
+              <h4>Mark Smith</h4>
+            </div>
+          </div>
+          <!-- .testimonial-panel end -->
+
+          <!-- Testimonial #3 -->
+          <div class="testimonial-panel">
+            <div class="testimonial--meta">
+              <div class="testimonial--meta-img">
+                <img src="<?= base_url() ?>assets/web_profile/images/testimonial/3.png" alt="Testimonial Author">
+              </div>
+            </div>
+            <!-- .testimonial-meta end -->
+            <div class="testimonial--body">
+              <p>“Proin gravida nibh vel velit auctor aliquet aenean sollidin, lorem quis bibendum auctor nisi elit.”</p>
+            </div>
+            <!-- .testimonial-body end -->
+            <div class="testimonial--meta-content">
+              <h4>Steve Martin</h4>
+            </div>
+          </div>
+          <!-- .testimonial-panel end -->
+
+          <!-- Testimonial #4 -->
+          <div class="testimonial-panel">
+            <div class="testimonial--meta">
+              <div class="testimonial--meta-img">
+                <img src="<?= base_url() ?>assets/web_profile/images/testimonial/2.png" alt="Testimonial Author">
+              </div>
+            </div>
+            <!-- .testimonial-meta end -->
+            <div class="testimonial--body">
+              <p>“Proin gravida nibh vel velit auctor aliquet aenean sollidin, lorem quis bibendum auctor nisi elit.”</p>
+            </div>
+            <!-- .testimonial-body end -->
+            <div class="testimonial--meta-content">
+              <h4>Smith Printz</h4>
+            </div>
+          </div>
+          <!-- .testimonial-panel end -->
+
+        </div>
+      </div>
+      <!-- .col-md-12 end -->
+    </div>
+    <!-- .row end -->
+  </div>
+  <!-- .container end -->
+</section>
+<!-- #testimonial2 end -->
+
+<!-- Blog Grid
+============================================= -->
+<section id="blog" class="blog blog-grid pb-100">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+        <div class="heading text--center mb-70">
+          <h2 class="heading--title">Galeri</h2>
+          <p>Ini adalah beberapa galeri foto dari barber shop kami</p>
           <div class="divider--line"></div>
         </div>
       </div>
@@ -427,141 +526,3 @@
   <!-- .container end -->
 </section>
 <!-- #clients1 end -->
-
-<!-- Footer #5
-============================================= -->
-<footer id="footer" class="footer">
-  <!-- Widget Section
-	============================================= -->
-  <div class="footer-widget">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-about">
-          <div class="footer--widget-content">
-            <img class="mb-20" src="<?= base_url() ?>assets/web_profile/images/logo/logo-light.png" alt="logo">
-            <p>Proin gravida nibh vel velit auctor aliquet anean lorem quis. bindum auctor, nisi elite conset ipsums sagtis id duis sed odio sit.</p>
-            <div class="work--schedule clearfix">
-              <ul class="list-unstyled">
-                <li>Monday - Friday <span>9.00 : 17.00</span></li>
-                <li>Saturday <span>9.00 : 15.00</span></li>
-                <li>Sunday <span>9.00 : 13.00</span></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <!-- .col-md-4 end -->
-        <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-recent">
-          <div class="footer--widget-title">
-            <h5>Latest Posts</h5>
-          </div>
-          <div class="footer--widget-content">
-            <div class="entry">
-              <div class="entry--img">
-                <a href="#">
-                  <img src="<?= base_url() ?>assets/web_profile/images/blog/thumb/5.jpg" alt="entry">
-                </a>
-              </div>
-              <div class="entry--content">
-                <div class="entry--title">
-                  <a href="#">Essential barbering tips you need to know before you start</a>
-                </div>
-                <div class="entry--meta">
-                  <span>Nov 09, 2017</span>
-                </div>
-              </div>
-            </div>
-            <!-- .entry end -->
-
-            <div class="entry">
-              <div class="entry--img">
-                <a href="#">
-                  <img src="<?= base_url() ?>assets/web_profile/images/blog/thumb/4.jpg" alt="entry">
-                </a>
-              </div>
-              <div class="entry--content">
-                <div class="entry--title">
-                  <a href="#">What are the 360 waves? and how you can get them</a>
-                </div>
-                <div class="entry--meta">
-                  <span>Oct 30, 2017</span>
-                </div>
-              </div>
-            </div>
-            <!-- .entry end -->
-
-            <div class="entry">
-              <div class="entry--img">
-                <a href="#">
-                  <img src="<?= base_url() ?>assets/web_profile/images/blog/thumb/3.jpg" alt="entry">
-                </a>
-              </div>
-              <div class="entry--content">
-                <div class="entry--title">
-                  <a href="#">Discover what is the best haircut for your face shape?</a>
-                </div>
-                <div class="entry--meta">
-                  <span>Oct 19, 2017</span>
-                </div>
-              </div>
-            </div>
-            <!-- .entry end -->
-          </div>
-        </div>
-        <!-- .col-md-4 end -->
-        <div class="col-xs-12 col-sm-6 col-md-4 footer--widget-contact">
-          <div class="footer--widget-title">
-            <h5>Get In Touch</h5>
-          </div>
-          <div class="footer--widget-content">
-            <ul class="list-unstyled mb-0">
-              <li><i class="fa fa-map-marker"></i> 1220 Petersham town, Wardll St New South Wales Australia PA 6550.</li>
-              <li><i class="fa fa-phone"></i> (04) 491 570 110</li>
-              <li><i class="fa fa-envelope-o"></i> contact@zytheme.com</li>
-            </ul>
-            <form class="form--newsletter">
-              <input type="email" name="email" class="form-control" placeholder="Email address">
-              <button type="submit">Subscribe</button>
-            </form>
-          </div>
-        </div>
-        <!-- .col-md-4 end -->
-      </div>
-    </div>
-    <!-- .container end -->
-  </div>
-  <!-- .footer-widget end -->
-  <!-- Copyrights
-	============================================= -->
-  <div class="footer--copyright">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6">
-          <span>&copy; 2017, All rights reserved.</span>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 text-right">
-          <div class="social">
-            <a class="share-twitter" href="#"><i class="fa fa-twitter"></i></a>
-            <a class="share-facebook" href="#"><i class="fa fa-facebook"></i></a>
-            <a class="share-linkedin" href="#"><i class="fa fa-linkedin"></i></a>
-            <a class="share-pinterest" href="#"><i class="fa fa-instagram"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- .container end -->
-  </div>
-</footer>
-</div>
-<!-- #wrapper end -->
-
-<!-- Footer Scripts
-============================================= -->
-<script src="<?= base_url() ?>assets/web_profile/js/jquery-2.2.4.min.js"></script>
-<script src="<?= base_url() ?>assets/web_profile/js/plugins.js"></script>
-<script src="<?= base_url() ?>assets/web_profile/js/functions.js"></script>
-</body>
-
-
-<!-- Mirrored from demo.zytheme.com/hairy/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Sep 2019 04:34:57 GMT -->
-
-</html>

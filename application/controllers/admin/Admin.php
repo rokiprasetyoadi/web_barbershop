1af13ea9f0c1dcaac68927ecb0f5f1093dc49577
ferdiannada <ferdian.nadda@gmail.com>
2019-10-15 05:44:34 +0700

    beautify

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -1,21 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
-
-	function __construct()
-	 	{
-	 		parent :: __construct();
-	 		if($this->session->userdata('status') != "login"){
-			redirect(base_url("admin/Login_admin"));
-			}
-	 	}
+class Admin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent :: __construct();
+        if ($this->session->userdata('status') != "login") {
+            redirect(base_url("admin/Login_admin"));
+        }
+    }
 
     public function index()
     {
         $this->temp->load('admin/partials', 'admin/admin');
     }
-
 }
 
 /* End of file Controllername.php */

9b59fb666f2941daf269f02d422af746362d84c5
Ferdian Nada <ferdiannada@gmail.com>
2019-10-19 19:11:18 +0700

    perubahan link shop.php ke shop/shop.php

--- application/views/partials.php
+++ application/views/partials.php
@@ -37,7 +37,13 @@
 </head>
 
 <body>
-
+    <div class="preloader">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
     <!-- Document Wrapper
 	============================================= -->
     <div id="wrapper" class="wrapper clearfix">

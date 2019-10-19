1364b8d37b47a7734aa871ec7200d359a339d946
Ferdian Nada <ferdiannada@gmail.com>
2019-10-19 15:50:04 +0700

    merubah isi readme

--- application/controllers/admin/Login_admin.php
+++ application/controllers/admin/Login_admin.php
@@ -45,26 +45,26 @@ class Login_admin extends CI_Controller
     }
 }
 =======
-<?php 
- 
+<?php
+
 class Login_admin extends CI_Controller{
- 
+
 	function __construct(){
-		parent::__construct();		
+		parent::__construct();
 		$this->load->model('M_loginadmin');
-		
+
 		// load Session Library
         $this->load->library('session');
-         
+
         // load url helper
         $this->load->helper('url');
- 
+
 	}
- 
+
 	function index(){
 		$this->load->view('admin/v_login_admin');
 	}
- 
+
 	function aksi_login(){
 		$admin_email = $this->input->post('admin_email');
 		$admin_password = $this->input->post('admin_password');
@@ -74,23 +74,23 @@ class Login_admin extends CI_Controller{
 			);
 		$cek = $this->M_loginadmin->cek_login("admin",$where)->row_array();
 		if($cek > 0){
- 
+
 			$data_session = array(
 				'admin_email' => $cek['admin_email'],
 				'admin_nama' => $cek['admin_nama'],
 				'admin_image' => $cek['admin_image'],
 				'status' => "login"
 				);
- 
+
 			$this->session->set_userdata($data_session);
- 
+
 			redirect(base_url("admin"));
- 
+
 		}else{
 			$this->load->view('admin/v_login_admin');
 		}
 	}
- 
+
 	function logout(){
 		$this->session->sess_destroy();
 		redirect(base_url('admin/Login_admin'));
--- application/controllers/admin/Member.php
+++ application/controllers/admin/Member.php
@@ -1,21 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Member extends CI_Controller {
-
-	function __construct()
-	 	{
-	 		parent :: __construct();
-	 		if($this->session->userdata('status') != "login"){
-			redirect(base_url("admin/Login_admin"));
-			}
-	 	}
+class Member extends CI_Controller
+{
+    public function __construct()
+    {
+        parent :: __construct();
+        if ($this->session->userdata('status') != "login") {
+            redirect(base_url("admin/Login_admin"));
+        }
+    }
 
     public function index()
     {
         $this->temp->load('admin/partials', 'admin/member');
     }
-
 }
 
 /* End of file Controllername.php */

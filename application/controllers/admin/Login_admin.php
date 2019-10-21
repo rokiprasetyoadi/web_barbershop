7e2c00ad0057c7fde7ff984f398ad3da13434070
panggilrofan <seesaajeen2@gmail.com>
2019-10-21 23:31:26 +0700

    Ngupdate wekku duewe

--- application/controllers/admin/Login_admin.php
+++ application/controllers/admin/Login_admin.php
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-class Login_admin extends CI_Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('M_loginadmin');
-    }
-
-    public function index()
-    {
-        $this->load->view('admin/v_login_admin');
-    }
-
-    public function aksi_login()
-    {
-        $crew_email = $this->input->post('crew_email');
-        $crew_password = $this->input->post('crew_password');
-        $where = array(
-            'crew_email' => $crew_email,
-            'crew_password' => md5($crew_password)
-            );
-        $cek = $this->M_loginadmin->cek_login("crew", $where)->row_array();
-        if ($cek > 0) {
-            $data_session = array(
-                'crew_email' => $cek['crew_email'],
-                'crew_nama' => $cek['crew_nama'],
-                'status' => "login"
-                );
-
-            $this->session->set_userdata($data_session);
-
-            redirect(base_url("admin"));
-        } else {
-            $this->load->view('admin/v_login_admin');
-        }
-    }
-
-    public function logout()
-    {
-        $this->session->sess_destroy();
-        redirect(base_url('admin/Login_admin'));
-    }
-}
-=======
 <?php
 
 class Login_admin extends CI_Controller{
@@ -96,4 +49,3 @@ class Login_admin extends CI_Controller{
 		redirect(base_url('admin/Login_admin'));
 	}
 }
->>>>>>> b71b3eadd173cc7309ad34404df301d758cae3e3

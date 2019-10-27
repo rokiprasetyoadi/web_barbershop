892032ee8e10d61805ebe7d5a8c97df279421398
Roki Prasetyo Adi <rokiprasetyoadi@gmail.com>
2019-10-27 19:32:02 +0700

    Back End
    Ngubah sitik favicon admin, gabut

--- application/views/admin/v_login_admin.php
+++ application/views/admin/v_login_admin.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="ThemeBucket">
-    <link rel="shortcut icon" href="images/favicon.html">
+    <link rel="shortcut icon" href="<?= base_url('assets/adm/images/logo_favicon.png') ?>">
 
     <title>SevenHead</title>
 
@@ -20,64 +20,27 @@
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url('assets/adm/css/style.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/adm/css/style-responsive.css') ?>" rel="stylesheet" />
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]>
-    <script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+    
 </head>
 
-  <body class="login-body">
+  <body class="login-body" style="background: #EFEFEF;">
 
     <div class="container">
 
       <form class="form-signin" method="post" action="<?php echo base_url('admin/Login_admin/aksi_login'); ?>">
         <h2 class="form-signin-heading"><b>Login</b></h2>
         <div class="login-wrap">
-            <div class="user-login-info">
+            <div class="user-login-info" style="background: white;">
               <?= $this->session->flashdata('pesan'); ?>
                 <input type="email" class="form-control" placeholder="Email" required="required" name="admin_email" autofocus>
                 <input type="password" class="form-control" placeholder="Password" required="required" name="admin_password" style="margin-top: 10px;">
-            </div>
+            
             <label class="checkbox">
                 <input type="checkbox" value="remember-me"> Remember me
-                <span class="pull-right">
-                    <a data-toggle="modal" href="#myModal"> Forgot Password?</a>
-
-                </span>
             </label>
             <button class="btn btn-lg btn-login btn-block" type="submit">Sign in</button>
-
-
-        </div>
-
-          <!-- Modal -->
-          <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
-              <div class="modal-dialog">
-                  <div class="modal-content">
-                      <div class="modal-header">
-                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                          <h4 class="modal-title">Forgot Password ?</h4>
-                      </div>
-                      <div class="modal-body">
-                          <p>Enter your e-mail address below to reset your password.</p>
-                          <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
-
-                      </div>
-                      <div class="modal-footer">
-                          <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
-                          <button class="btn btn-success" type="button">Submit</button>
-                      </div>
-                  </div>
-              </div>
           </div>
-          <!-- modal -->
-
+        </div>
       </form>
 
     </div>

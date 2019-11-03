3b1ae76304deed06930298091aa49f38a4662864
Roki Prasetyo Adi <rokiprasetyoadi@gmail.com>
2019-11-03 19:39:17 +0700

    Back End
    Tambah data barang

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','temp','session');
+$autoload['libraries'] = array('database','temp','session','form_validation');
 
 /*
 | -------------------------------------------------------------------
--- application/controllers/admin/Barang.php
+++ application/controllers/admin/Barang.php
@@ -18,6 +18,20 @@ class Barang extends CI_Controller {
         $this->temp->load('admin/partials', 'admin/barang', $data);
     }
 
+    public function add()
+    {
+        $this->M_barang->rulesNew();
+        $data['kode'] = $this->M_barang->kode();
+        
+        if ($this->form_validation->run() == false) {
+            $this->temp->load('admin/partials', 'admin/form_barang', $data);
+        } else {
+            $this->M_barang->addNew();
+            $this->session->set_flashdata('pesan', '<div class="alert alert-outline alert-success">Data berhasil ditambahkan!<button type="button" class="close" data-dismiss="alert">&times;</button></div>');
+            redirect('admin/barang');
+        }
+    }
+
 }
 
 /* End of file Controllername.php */

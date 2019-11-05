9499578c361e2a15da9956fb0e8f87bfb6ab0dee
Ferdian Nada <ferdiannada@gmail.com>
2019-11-06 00:30:38 +0700

    gak ada

--- application/controllers/admin/Barang.php
+++ application/controllers/admin/Barang.php
@@ -1,23 +1,22 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Barang extends CI_Controller {
-
-	function __construct()
-	 	{
-	 		parent :: __construct();
-	 		if($this->session->userdata('status') != "login"){
-			redirect(base_url("admin/Login_admin"));
-			}
-			$this->load->model("M_barang");
-	 	}
+class Barang extends CI_Controller
+{
+    public function __construct()
+    {
+        parent :: __construct();
+        if ($this->session->userdata('status') != "login") {
+            redirect(base_url("admin/Login_admin"));
+        }
+        $this->load->model("M_barang");
+    }
 
     public function index()
     {
         $data['tbl_barang']=$this->M_barang->tampil_data();
         $this->temp->load('admin/partials', 'admin/barang', $data);
     }
-
 }
 
 /* End of file Controllername.php */
